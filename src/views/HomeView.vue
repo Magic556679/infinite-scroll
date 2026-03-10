39f5a989e1fe72c5b03c0c220603ca8371b42800
李晏瑋 <magic556679@gmail.com>
2026-03-10 17:31:07 +0800

    feat: 串接 repos API

---
+++ src/views/HomeView.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="wrap">
+    <h1>Vuejs Repositories</h1>
+
+    <div ref="viewArea">
+      <div v-for="(item, index) in reposData" :key="item.id">
+        <div class="my-1 border p-1 rounded-2xl">
+          <span>index: {{ index }}</span>
+          <h3>{{ item.name }}</h3>
+          <p>{{ item.description }}</p>
+          <a :href="item.html_url" target="_blank">repo url</a>
+        </div>
+      </div>
+    </div>
+    <div ref="bottom"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const reposName = 'vuejs'
+const perPage = ref(10) // default 30
+const page = ref(1)
+
+interface ReposData {
+  name: string
+  id: number
+  description: string
+  html_url: string
+}
+const reposData = ref<ReposData[]>([])
+
+const getReposData = async () => {
+  const res = await fetch(
+    `https://api.github.com/users/${reposName}/repos?per_page=${perPage.value}&page=${page.value}`,
+  )
+
+  reposData.value = await res.json()
+}
+
+onMounted(() => {
+  getReposData()
+})
+</script>
+
+<style scoped>
+.wrap {
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.border {
+  border: 1px solid;
+}
+
+.my-1 {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+.p-1 {
+  padding: 1rem;
+}
+
+.rounded-2xl {
+  border-radius: 1rem;
+}
+</style>
